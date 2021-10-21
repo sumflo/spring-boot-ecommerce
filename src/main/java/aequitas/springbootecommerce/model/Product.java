2fcb5870df73830eb97ebbc3659cf215891ec45b
sumflo <real61madrid@gmail.com>
2021-10-21 02:36:49 +0200

    entity update

--- src/main/java/aequitas/springbootecommerce/model/Product.java
+++ src/main/java/aequitas/springbootecommerce/model/Product.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -21,6 +23,10 @@ public class Product {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name="id")
   private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id", nullable = false)
+  private ProductCategory category;
 
   @Column(name="sku")
   private String sku;
